140cbbd5a066a381188e354dece972083c7fb2c9
Tim Gates <tgates@iress.com.au>
2017-08-30 21:37:19 +1100

    Emergency rollback, broke the build
    Forgot to push a new docker image
    
    Revert "Switch to django"
    
    This reverts commit 42935e690babbb4ad457377bef5fab5ed6c73e94.

--- docker/app.py
+++ docker/app.py
@@ -1,49 +1,25 @@
-#!/usr/bin/env python
-"""\
-%(app)s
-
-Usage:
-   %(cmd)s [options]
-   %(cmd)s (-h | --help)
-   %(cmd)s --version
-
-Options:
-  -h --help              Show this screen
-  --version              Show version
-
-"""
-# vi:syntax=python
-
-from __future__ import print_function, division
-
-import sys
+from flask import Flask
+from redis import Redis, RedisError
 import os
+import socket
 
-__version__ = '0.1'
-__appname__ = None
+# Connect to Redis
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
-def main():
-    """
-    Read command line options and pass to the main command line entry point.
-    """
-    run_main()
+app = Flask(__name__)
 
-def get_basedir():
-    """
-    Locate the current directory of this file
-    """
-    return os.path.dirname(os.path.abspath(sys.modules[__name__].__file__))
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-def run_main():
-    """
-    Main Command Line Entry Point
-    """
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sampleapp.settings")
-    sys.path.append(os.path.join(get_basedir(), 'sampleapp'))
-    from django.core.management import execute_from_command_line
-    execute_from_command_line([sys.argv[0], 'runserver', '0.0.0.0:80'])
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
 
-# vim: set ft=python:
