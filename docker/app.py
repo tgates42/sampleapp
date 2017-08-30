42935e690babbb4ad457377bef5fab5ed6c73e94
Tim Gates <tgates@iress.com.au>
2017-08-30 18:31:49 +1100

    Switch to django

--- docker/app.py
+++ docker/app.py
@@ -1,25 +1,49 @@
-from flask import Flask
-from redis import Redis, RedisError
+#!/usr/bin/env python
+"""\
+%(app)s
+
+Usage:
+   %(cmd)s [options]
+   %(cmd)s (-h | --help)
+   %(cmd)s --version
+
+Options:
+  -h --help              Show this screen
+  --version              Show version
+
+"""
+# vi:syntax=python
+
+from __future__ import print_function, division
+
+import sys
 import os
-import socket
 
-# Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+__version__ = '0.1'
+__appname__ = None
 
-app = Flask(__name__)
+def main():
+    """
+    Read command line options and pass to the main command line entry point.
+    """
+    run_main()
 
-@app.route("/")
-def hello():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
+def get_basedir():
+    """
+    Locate the current directory of this file
+    """
+    return os.path.dirname(os.path.abspath(sys.modules[__name__].__file__))
 
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+def run_main():
+    """
+    Main Command Line Entry Point
+    """
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sampleapp.settings")
+    sys.path.append(os.path.join(get_basedir(), 'sampleapp'))
+    from django.core.management import execute_from_command_line
+    execute_from_command_line([sys.argv[0], 'runserver', '0.0.0.0:80'])
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+if __name__ == '__main__':
+    main()
 
+# vim: set ft=python:
