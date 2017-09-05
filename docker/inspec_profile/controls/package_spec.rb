07a420abf32afb276d1bf1665534e8202eebb197
Michael Robbins <michael.robbins@nebulr.com.au>
2017-09-05 11:04:06 +1000

    Renamed file and fixed it to test the software exists vs the package (as it's built from source)

--- docker/inspec_profile/controls/package_spec.rb
+++
@@ -1,7 +0,0 @@
-control "package-01" do
-  impact 1.0
-  title "Python should be installed"
-  describe package('python') do
-    it { should be_installed }
-  end
-end
---
+++ docker/inspec_profile/controls/software_spec.rb
@@ -0,0 +1,8 @@
+control "software-01" do
+  impact 1.0
+  title "Python should be available"
+  describe command('python2 --version') do
+    its('stdout') { should match /^Python 2.7.[0-9]+$/ }
+    its('exit_status') { should eq 0 }
+  end
+end
